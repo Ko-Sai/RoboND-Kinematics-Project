c03a305beb682ba57eaf1c8721bde5fb863bbc55
Sai <kudousaihtet@gmail.com>
2019-02-13 15:07:31 +0630

    Updated

--- IK_debug.py
+++ IK_debug.py
@@ -64,12 +64,129 @@ def test_code(test_case):
 
     ## Insert IK code here!
     
-    theta1 = 0
-    theta2 = 0
-    theta3 = 0
-    theta4 = 0
-    theta5 = 0
-    theta6 = 0
+    def rot_x(q):
+	R_x = Matrix([[1, 0,      0       ],
+	              [0, cos(q), -sin(q) ],
+	              [0, sin(q), cos(q) ]])
+
+	return R_x
+
+    def rot_y(q):
+	R_y = Matrix([[cos(q),  0, sin(q)],
+	              [0,       1,      0],
+	             [-sin(q), 0, cos(q)]])
+        return R_y
+
+    def rot_z(q):
+        R_z = Matrix([[cos(q), -sin(q), 0],
+                      [sin(q), cos(q),  0],
+	              [0,      0,       1]])
+        return R_z
+
+    ### Creates Homogeneous Transform Matrix
+    def tf_matrix(q, d, a, alpha):
+	T = Matrix([[            cos(q),           -sin(q),           0,             a],
+	            [ sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
+	            [ sin(q)*sin(alpha), cos(q)*sin(alpha),  cos(alpha),  cos(alpha)*d],
+	            [                 0,                 0,           0,             1]])
+        return T
+
+
+    q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
+    # Link offset (variable for prismatic)
+    d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+    # Link length
+    a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+    # Twist angles
+    alpha0,alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
+
+    # Create Modified DH parameters
+    DH = {alpha0:     0,  a0:      0,  d1:   0.75,  q1:      q1,
+          alpha1: -pi/2,  a1:   0.35,  d2:      0,  q2: q2-pi/2,
+          alpha2:     0,  a2:   1.25,  d3:      0,  q3:      q3,
+          alpha3: -pi/2,  a3: 0.0536,  d4: 1.5014,  q4:      q4,
+          alpha4:  pi/2,  a4:      0,  d5:      0,  q5:      q5,
+          alpha5: -pi/2,  a5:      0,  d6:      0,  q6:      q6,
+          alpha6:     0,  a6:      0,  d7:  0.303,  q7:       0}
+
+    # Create individual transformation matrices
+    T0_1  = tf_matrix(q1, d1, a0, alpha0).subs(DH)
+    T1_2  = tf_matrix(q2, d2, a1, alpha1).subs(DH)
+    T2_3  = tf_matrix(q3, d3, a2, alpha2).subs(DH)
+    T3_4  = tf_matrix(q4, d4, a3, alpha3).subs(DH)
+    T4_5  = tf_matrix(q5, d5, a4, alpha4).subs(DH)
+    T5_6  = tf_matrix(q6, d6, a5, alpha5).subs(DH)
+    T6_EE = tf_matrix(q7, d7, a6, alpha6).subs(DH)
+
+    # Extract rotation matrices from the transformation matrices
+    # Generalized homogeneous transform
+    T0_EE = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_EE
+
+
+    px = req.poses[x].position.x
+    py = req.poses[x].position.y
+    pz = req.poses[x].position.z
+
+    (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
+         [req.poses[x].orientation.x, req.poses[x].orientation.y,
+           req.poses[x].orientation.z, req.poses[x].orientation.w])
+
+    ### Your IK code here
+    # Compensate for rotation discrepancy between DH parameters and Gazebo
+
+    r, p, y = symbols('r p y')
+    R_x = rot_x(r)
+    R_y = rot_y(p)
+    R_z = rot_z(y)
+
+    R_corr = R_z.subs(y, pi) * R_y.subs(p, -pi/2)
+    Rrpy = R_z * R_y * R_x * R_corr
+    Rrpy = Rrpy.subs({'r': roll, 'p': pitch, 'y': yaw})
+
+    # Calculate joint angles using Geometric IK method
+    # Calculate the wrist center position first
+    nx = Rrpy[0,2]
+    ny = Rrpy[1,2]
+    nz = Rrpy[2,2]
+
+    # d7 = 0.303
+    wx =  px - 0.303 * nx
+    wy =  py - 0.303 * ny
+    wz =  pz - 0.303 * nz
+ 
+    # Theta 1 from above 
+    theta1 = atan2(wy, wx)
+
+    # Calculate Radius from above
+    r = sqrt(wx**2+wy**2) - 0.35 # a1: 0.35
+
+    # Calculating Theta 2 and 3 using cosine law. A, B and C are sides of the triangle
+    A = 1.5014 # d4
+    B = sqrt(r**2+(wz-0.75)**2) # d1: 0.75
+    C = 1.25 # a2
+
+    # a corresponds to angle apha
+    a = acos((B**2 + C**2 - A**2) / (2*B*C))
+    theta2 = pi/2 - a - atan2(wz-0.75, r)  # d1: 0.75
+
+    # b corresponds to angle beta
+    b = acos((A**2 + C**2 - B**2) / (2*A*C))
+    theta3 = pi/2 - (b + 0.036) # 0.036 accounts for sag in link4 of -0.054m
+
+    # Calculating Euler angles from orientation
+    R0_3 = T0_1[0:3,0:3] * T1_2[0:3,0:3] * T2_3[0:3,0:3]
+    R0_3 = R0_3.evalf(subs={'q1':theta1, 'q2':theta2, 'q3':theta3})
+
+    #R3_6 = R0_3.inv("LU")*Rrpy # using transpose instead:
+    R3_6 = R0_3.T * Rrpy
+
+    theta4 = atan2(R3_6[2, 2], -R3_6[0, 2])
+    theta5 = atan2(sqrt(R3_6[0, 2]**2+R3_6[2, 2]**2), R3_6[1, 2])
+    theta6 = atan2(-R3_6[1, 1], R3_6[1, 0])
+
+    print('<Theras location: (%s, %s, %s, %s, %s, %s)' % (theta1, theta2, theta3, theta4, theta5, theta6))
+
+
 
     ## 
     ########################################################################################
@@ -79,13 +196,15 @@ def test_code(test_case):
     ## as the input and output the position of your end effector as your_ee = [x,y,z]
 
     ## (OPTIONAL) YOUR CODE HERE!
+    FK= (T0_EE).evalf(subs={q1: theta1, q2: theta2, q3: theta3, q4: theta4, q5: theta5, q6: theta6})
 
     ## End your code input for forward kinematics here!
     ########################################################################################
 
     ## For error analysis please set the following variables of your WC location and EE location in the format of [x,y,z]
-    your_wc = [1,1,1] # <--- Load your calculated WC values in this array
-    your_ee = [1,1,1] # <--- Load your calculated end effector value from your forward kinematics
+    your_wc = [wx,wy,wz] # <--- Load your calculated WC values in this array
+    print(your_wc)
+    your_ee = [FK[0, 3],FK[1, 3],FK[2, 3]] # <--- Load your calculated end effector value from your forward kinematics
     ########################################################################################
 
     ## Error analysis
